924fd806f0fa1456808fcd878017bcab06574bd6
Chris Basham <chris@bash.am>
2020-11-20 15:36:34 -0500

    Update github2synology.sh

--- github2synology.sh
+++ github2synology.sh
@@ -12,9 +12,10 @@ BACKUP_PATH="/volume1/serverBackups/github/backup"
 COUNTER=100
 TOTALCOUNTER=0
 PAGE=1
-GIT="c//volume1/@appstore/git/bin/git"
+# git path
+GIT="git"
 fetch_fromUrl() {
-	COUNTER=0
+    COUNTER=0
     API_URL="https://api.github.com/user/repos?type=all&per_page=100&page=${PAGE}"
     echo "Fetching from ${API_URL}"
     REPOS=`curl -H "Authorization: token ${OAUTH_TOKEN}" -s "${API_URL}" | jq -r 'values[] | "\(.full_name),\(.private),\(.git_url),\(.has_wiki)"'`
@@ -26,33 +27,29 @@ fetch_fromUrl() {
         PRIVATEFLAG=`echo ${REPO} | cut -d ',' -f2`
         ORIGINALGITURL=`echo ${REPO} | cut -d ',' -f3`
         HASWIKI=`echo ${REPO} | cut -d ',' -f4`
-        GITURL="${ORIGINALGITURL/git:\/\/github.com\//git@github.com:}"
-        mkdir "${BACKUP_PATH}/${REPONAME}" -p
-        REPOPATH="${BACKUP_PATH}/${REPONAME}/code"
+        GITURL="https://${OAUTH_TOKEN}@github.com/${REPONAME}.git"
+        REPOPATH="${BACKUP_PATH}/${REPONAME}"
         if [ -d "$REPOPATH" ]; then
             echo "PULLING Repo URL: ${REPONAME} from url ${GITURL} to ${REPOPATH}"
             cd ${REPOPATH}
-            ${GIT} pull
+            ${GIT} pull --no-rebase
         else
             echo "CLONING Repo URL: ${REPONAME} from url ${GITURL} to ${REPOPATH}"
             ${GIT} clone ${GITURL} ${REPOPATH}
-            if [ "true"===${PRIVATEFLAG} ]; then
-                `touch ${BACKUP_PATH}/${REPONAME}/private`
-            fi
         fi
-        if [ "true"===${HASWIKI} ]; then
-            WIKIPATH="${BACKUP_PATH}/${REPONAME}/wiki"
-            WIKIURL="${ORIGINALGITURL/git:\/\/github.com\//git@github.com:}"
-            WIKIURL=`echo ${WIKIURL} | sed -e "s/.git$/.wiki.git/"`
+        if [ "true" == ${HASWIKI} ]; then
+            WIKIPATH="${BACKUP_PATH}/${REPONAME}.wiki"
+            WIKIURL="https://${OAUTH_TOKEN}@github.com/${REPONAME}.wiki.git"
             if [ -d "$WIKIPATH" ]; then
                 echo "PULLING Repo Wiki: ${REPONAME} from url ${WIKIURL}: to ${WIKIPATH}"
                 cd ${WIKIPATH}
-                ${GIT} pull
+                ${GIT} pull --no-rebase
             else
                 echo "CLONING Repo Wiki: ${REPONAME} from url ${WIKIURL}:to ${WIKIPATH}"
                 ${GIT} clone ${WIKIURL} ${WIKIPATH}
             fi
         fi
+        echo ""
     done
 }
 until [ $COUNTER -lt 100 ]; do
